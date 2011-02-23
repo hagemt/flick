12d61d66a2006f9906ba7d71b419004b3eabf74c
Asher Glick <asher.glick@gmail.com>
2011-02-22 19:44:56 -0500

    basic code update

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+#include"tcp.h"
+#include"udp.h"
+using namespace std;
+int mian() {
+  init();
+  cout << "Welcome to flick CLI" << endl;
+  string input;
+  
+  while (true) {
+    cin >> input;
+    if (
+  }
+}
---
+++ tcp.h
@@ -0,0 +1,9 @@
+#ifndef tcp_h_
+#define tcp_h_
+/*
+  This file contains the methods of
+   initilizing a tcp socket server
+   initilizing a tcp socket client
+   sending data over a socket
+*/
+#endif
