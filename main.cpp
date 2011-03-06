a56925198cd0c4bf64ea89b1ccc693bd67e53ca8
Asher Glick <asher.glick@gmail.com>
2011-03-06 02:24:44 -0500

    tcp server config function added

--- main.cpp
+++ main.cpp
@@ -1,15 +1,61 @@
-#include<iostream>
-#include<string>
-#include"tcp.h"
-#include"udp.h"
+/*
+  This code is written by Asher Glick and Tor Hagemann
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include "tcp.h" // TCP protocols
+#include "udp.h" // UDP protocols
 using namespace std;
-int mian() {
-  init();
+
+struct remoteUser;
+
+// Function Prototypes
+vector <remoteUser> scanNetwork();
+
+// Main function
+int main() {
+  // Setup configurations
+    // fork udp ping program
+    // initilize tcp socket listner 
+  
+  //  Begin UI
   cout << "Welcome to flick CLI" << endl;
   string input;
   
+  vector <remoteUser> activeUserList;
+  vector <remoteUser> connectedUserList;
+  
   while (true) {
-    cin >> input;
-    if (
+    getline(cin,input);
+    if (input == "Scan Network") {
+      activeUserList = scanNetwork();
+    }
+    
+    else {
+      cout << "Functions that can be used are:"<< endl;
+      cout << " scan network" << endl;
+      //cout << " connect to computer" << endl;
+      //cout << " send window" << endl;
+      //cout << " take all windows back" << endl;
+    }
   }
+}
+
+/*
+  remote user struct
+*/
+struct remoteUser {
+  string hostname;
+  string ipaddress;
+};
+
+/* Scan Network *\
+|  
+\*/
+vector <remoteUser> scanNetwork() {
+  // send message out to the broadcast address
+    // wait for an ammount of time untill you get messages form the forked server process (on the remote machines
+    // populate the remote user list variable with the responces
+    // return the remote user list
 }
